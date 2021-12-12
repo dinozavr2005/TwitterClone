4ed97e6b07f86280fa313195b41c95293b321184
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2021-12-12 21:21:08 +0300

    2

---
+++ TwitterClone/Controllers/Authentication/LoginController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginController.swift
+//  TwitterClone
+//
+//  Created by Владимир on 10.12.2021.
+//  Copyright © 2021 LEADGROUP. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = #imageLiteral(resourceName: "TwitterLogo")
+        return iv
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - Selector
+
+    // MARK: - Helper
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+        logoImageView.setDimensions(width: 150, height: 150)
+    }
+
+}
---
+++ TwitterClone/Controllers/Authentication/RegistrationController.swift
@@ -0,0 +1,31 @@
+//
+//  RegistrationController.swift
+//  TwitterClone
+//
+//  Created by Владимир on 10.12.2021.
+//  Copyright © 2021 LEADGROUP. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - Selector
+
+    // MARK: - Helper
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+
+}
