83e1935fec5269f0908a83a36e639f1712c8a0b1
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2021-12-24 14:15:47 +0300

    User Profile

---
+++ TwitterClone/ViewModel/ProfileHeaderViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  ProfileHeaderViewModel.swift
+//  TwitterClone
+//
+//  Created by Владимир on 23.12.2021.
+//  Copyright © 2021 LEADGROUP. All rights reserved.
+//
+
+import UIKit
+
+
+enum ProfileFilterOptions: Int, CaseIterable {
+    case tweets
+    case replies
+    case likes
+    
+    var description: String {
+        switch self {
+        case .tweets: return "Tweets"
+        case .replies: return "Tweets & Replies"
+        case .likes: return "Likes"
+        }
+    }
+}
+
+struct ProfileHeaderViewModel {
+    
+    private let user: User
+    
+    let usernameText: String
+    
+    var followersString: NSAttributedString? {
+        return attributedText(withValue: 0, text: "followers")
+    }
+    
+    var followingString: NSAttributedString? {
+        return attributedText(withValue: 2, text: "following")
+    }
+    
+    var actionButtonTitle: String {
+        if user.isCurrentUser {
+            return "Edit Profile"
+        } else {
+            return "Follow"
+        }
+    }
+    
+    init(user: User) {
+        self.user = user
+        
+        self.usernameText = "@" + user.username
+    }
+    
+    fileprivate func attributedText(withValue value: Int, text: String) -> NSAttributedString {
+        let attributedTitle = NSMutableAttributedString(string: "\(value)",
+                                                        attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedTitle.append(NSAttributedString(string: " \(text)",
+                                                  attributes: [.font: UIFont.systemFont(ofSize: 14),
+                                                                .foregroundColor: UIColor.lightGray ]))
+        return attributedTitle
+    }
+    
+}
