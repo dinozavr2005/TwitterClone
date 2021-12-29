2454786b7391c981918dc7d82b3c5ca047d6a702
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2021-12-29 17:54:55 +0300

    likes tweets

--- TwitterClone/Controllers/FeedController.swift
+++ TwitterClone/Controllers/FeedController.swift
@@ -45,6 +45,18 @@ class FeedController: UICollectionViewController {
     func fetchTweets() {
         TweetService.shared.fetchTweets { tweets in
             self.tweets = tweets
+            self.checkIfUserLikesTweets(tweets)
+
+        }
+    }
+    
+    func checkIfUserLikesTweets(_ tweets: [Tweet]) {
+        for (index, tweet) in tweets.enumerated() {
+            TweetService.shared.checkIfUserLikedTweet(tweet) { didLike in
+                guard didLike == true else { return }
+                
+                self.tweets[index].didLike = true
+            }
         }
     }
 
@@ -113,9 +125,17 @@ extension FeedController: UICollectionViewDelegateFlowLayout {
 // MARK: - TweetCellDelegeate
 
 extension FeedController: TweetCellDelegate {
+    func handleLikeTapped(_ cell: TweetCell) {
+        guard let tweet = cell.tweet else { return }
+        TweetService.shared.likeTweet(tweet: tweet) { (err, ref) in
+            cell.tweet?.didLike.toggle()
+            let likes = tweet.didLike ? tweet.likes - 1 : tweet.likes + 1
+            cell.tweet?.likes = likes
+        }
+    }
+    
     func handleReplyTapped(_ cell: TweetCell) {
         guard let tweet = cell.tweet else { return }
-        
         let controller = UploadTweetController(user: tweet.user, config: .reply(tweet))
         let nav = UINavigationController(rootViewController: controller)
 //        nav.modalPresentationStyle = .automatic
